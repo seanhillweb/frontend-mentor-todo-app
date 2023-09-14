b03efe580eb135e806d78ee9c82645ec4cb50960
seanhillweb <seanhillweb@gmail.com>
2023-09-14 14:36:51 -0500

    feat: configuration setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,52 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: {
+          "bright-blue": "#3a7bfd",
+          "gradient-start": "#57ddff",
+          "gradient-end": "#c058f3",
+        },
+        scheme: {
+          light: {
+            100: "#fafafa",
+            200: "#e4e5f1",
+            300: "#d2d3db",
+            400: "#9394a5",
+            500: "#484b6a",
+          },
+          dark: {
+            100: "#161722",
+            200: "#25273c",
+            300: "#cacde8",
+            400: "#e4e5f1",
+            500: "#777a92",
+            600: "#4d5066",
+            700: "#393a4c",
+          },
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    // require("@headlessui/tailwindcss")({ prefix: "ui" }),
+  ],
+};
